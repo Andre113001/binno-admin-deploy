81ae2d88d5b40c97def8a825ae0156a7b8aa249c
Andre113001 <1130marcusa@gmail.com>
2024-01-09 03:53:33 +0800

    Updated index

--- api/index.js
+++ api/index.js
@@ -17,6 +17,13 @@ app.use(corsMiddleware);
 app.use(jsonMiddleware);
 app.use(urlencodedMiddleware);
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // Import Route Files
 const getRoute = require('./routes/getRoute');
 const loginRoute = require('./routes/loginRoute');
