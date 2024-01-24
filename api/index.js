9851cbedb08ac0dc1e1a8ca64b693532b1abf086
Andre113001 <1130marcusa@gmail.com>
2024-01-24 14:37:24 +0800

    updated cors

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(urlencodedMiddleware)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
     next()
 })
 
