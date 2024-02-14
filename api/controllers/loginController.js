20668171835b34af75b32fc27c5d0b070317c213
alexxShandsome <algilbertgomez@gmail.com>
2024-02-14 12:30:59 +0800

    refactor: new database query for loginController.js

--- api/controllers/loginController.js
+++ api/controllers/loginController.js
@@ -13,41 +13,45 @@ const authenticateUser = async (accessKey, password) => {
     const hashedAccesskey = hash(accessKey).toString('base64');
 
     return new Promise((resolve, reject) => {
-        try {    
-            db.query(
-                `SELECT * FROM admin_i WHERE admin_accessKey = ?`,
-                [hashedAccesskey],
-                async (err, result) => {
-                    if (err) {
-                        reject({ error: 'Internal server error' });
-                    }
-
-                    if (!result[0].hasOwnProperty('admin_pass')) {
-                        resolve({ error: 'User not found' });
-                    }
-
-                    const DBpassword = result[0].admin_pass;
-
-                    if (!DBpassword) {
-                        resolve({ error: 'User password not found' });
-                    }
-
-                    const passwordMatch = await bcrypt.compare(password, DBpassword);
-
-                    if (passwordMatch) {
-                        const user = result[0];
-
-                        const token = jwt.sign(
-                            { userId: user.account_id, username: user.name },
-                            process.env.JWT_SECRET_KEY,
-                            { expiresIn: '1h' }
-                        );
-                        
-                        resolve({ token });
-                    } else {
-                        resolve({ error: 'Authentication failed' });
-                    }
+        try {
+            const getAdminProfileByAccessKeyQuery = `
+                SELECT * FROM admin_i WHERE admin_accessKey = ?
+            `;
+            // NOTE: new query for the new database - AL
+            // const getAdminProfileByAccessKeyQuery = `
+            //     SELECT * FROM admin_profile WHERE access_key = ?
+            // `;
+            db.query(getAdminProfileByAccessKeyQuery, [hashedAccesskey], async (err, result) => {
+                if (err) {
+                    reject({ error: 'Internal server error' });
                 }
+
+                if (!result[0].hasOwnProperty('admin_pass')) {
+                    resolve({ error: 'User not found' });
+                }
+
+                const DBpassword = result[0].admin_pass;
+
+                if (!DBpassword) {
+                    resolve({ error: 'User password not found' });
+                }
+
+                const passwordMatch = await bcrypt.compare(password, DBpassword);
+
+                if (passwordMatch) {
+                    const user = result[0];
+
+                    const token = jwt.sign(
+                        { userId: user.account_id, username: user.name },
+                        process.env.JWT_SECRET_KEY,
+                        { expiresIn: '1h' }
+                    );
+
+                    resolve({ token });
+                } else {
+                    resolve({ error: 'Authentication failed' });
+                }
+            }
             );
         } catch (error) {
             console.log(error);
